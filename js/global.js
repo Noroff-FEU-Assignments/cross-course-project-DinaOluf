c29027ad0f5100ca2b4c72371a662ec2a774ab7f
DinaOluf <91533917+DinaOluf@users.noreply.github.com>
2022-03-20 12:11:08 +0100

    Options button dropdown fixed with Javascript, all pages

--- js/global.js
+++ js/global.js
@@ -1,36 +1,41 @@
 const optionsButton = document.querySelector("#options_icon");
-const hiddenMenu = document.querySelector(".hidden-menu");
+const hiddenMenu = document.querySelector("#hidden-menu");
 
 optionsButton.onclick = function showOptions() {
-    hiddenMenu.classList.add("options-menu");
-    hiddenMenu.innerHTML = `<div class="topic">
-        <div class="menu-heading">
-            <img src="/images/Controller.png"></img>
-            <h3>Games</h3>
-        </div>
-        <ul>
-             <li><a href="../newgames.html">Buy New Games</a></li>
-            <li><a href="../usedgames.html">Buy Used Games</a></li>
-            <li><a href="../preorder.html">Pre-Order Games</a></li>
-            <li><a href="../sellgames.html">Sell Your Games</a></li>
-        </ul>
-    </div>
-    <div class="topic">
-        <a href="../contact.html">
-            <div class="menu-heading">
-                <img src="/images/Mail-icon.png"></img>
-                <h3>Contact us</h3>
-            </div>
-        </a>
-        <ul>
-            <li>Service@GameHub.com</li>
-            <li>+1234567890</li>
-        </ul>
-    </div>`
+    const menuClassList = hiddenMenu.classList;
 
-    if (hiddenMenu.classlist.length === 2) {
-        hiddenMenu.classList.remove("options-menu");
+    if (menuClassList.length === 1) {
+        menuClassList.remove("options-menu");
         hiddenMenu.innerHTML = "";
+    } else {
+        hiddenMenu.classList.add("options-menu");
+        addMenuHtml();
     }
 };
 
+function addMenuHtml() {
+    hiddenMenu.innerHTML = `<div class="topic">
+    <div class="menu-heading">
+        <img src="/images/Controller.png"></img>
+        <h3>Games</h3>
+    </div>
+    <ul>
+         <li><a href="../newgames.html">Buy New Games</a></li>
+        <li><a href="../usedgames.html">Buy Used Games</a></li>
+        <li><a href="../preorder.html">Pre-Order Games</a></li>
+        <li><a href="../sellgames.html">Sell Your Games</a></li>
+    </ul>
+</div>
+<div class="topic">
+    <a href="../contact.html">
+        <div class="menu-heading">
+            <img src="/images/Mail-icon.png"></img>
+            <h3>Contact us</h3>
+        </div>
+    </a>
+    <ul>
+        <li>Service@GameHub.com</li>
+        <li>+1234567890</li>
+    </ul>
+</div>`
+}
